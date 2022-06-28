7af07de94d82927c4f05d2d54fcb6d6973af80fd
Roman Pchelkin <epos.91@mail.ru>
2022-06-28 10:02:13 +0300

    upd-1

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const timeDiv = document.querySelector('.clock');
 const weatherDiv = document.querySelector('.weather');
 const bodyDiv = document.querySelector('.body_container');
 
-let updTime = setInterval(() => timeUpd(), 1000);
 timeUpd();
 weatherUpd();
+let updTime = setInterval(() => timeUpd(), 1000);
+
 
 function timeUpd() {
 	let time = new Date();
@@ -50,20 +51,8 @@ function timeUpd() {
 	if (second < 10) {
 		second = '0' + second;
 	}
-
-	// if (time.getHours() > 8 || time.getHours() < 22 ) {
-		// bodyDiv.classList.remove('night');
-		// bodyDiv.classList.add('day');
 		timeDiv.innerHTML = `
-		<h2>${date.toLocaleDateString()}</h2><br><h1 class="date-time">${dayOfWeek}, ${hour}:${minute}:${second}</h1>`;
-		
-	// } else {
-	// 	bodyDiv.classList.remove('day');
-	// 	bodyDiv.classList.add('night');
-	// 	timeDiv.innerHTML = `
-	// 	<h2>${date.toLocaleDateString()}</h2><br><h1 class="date-time">${dayOfWeek}, ${hour}:${minute}:${second}</h1>`;
-	// }
-
+		<h2>${date.toLocaleDateString()}</h2><h1 class="date-time">${dayOfWeek}, ${hour}:${minute}:${second}</h1>`;
 }
 
 function weatherUpd() {
@@ -72,21 +61,15 @@ function weatherUpd() {
 	let location;
 	let condition;
 	let condIcon;
-	let conditionTranslate = '';
 	fetch('https://api.weatherapi.com/v1/current.json?key=f1c58ace0a7b44a1b8b92424222206&q=Москва&aqi=no&lang=ru')
 		.then((response) => response.json())
 		.then(function (data) {
 			location = data.location.name;
 			temp = data.current.temp_c;
 			condition = data.current.condition.text;
-			// switch (condition) {
-			// 	case 'Sunny': conditionTranslate = 'Солнечно';
-			// 	break;
-
-			// }
 			condIcon = data.current.condition.icon;
 			weatherDiv.innerHTML = `
-			<h2>${location}, ${temp}°<br>${condition} <img class="cond-icon" src="${condIcon}" alt=""></img></h2>
+			<h2 style="transform: translateX(10%);">${location}, +${temp}°<br>${condition}</h2><img style="transform: translateX(10%);" class="cond-icon" src="${condIcon}" alt=""></img>
 		`;
 		});
 }
